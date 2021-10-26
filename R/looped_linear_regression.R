ab5086d2b736b609f8c53419de0f27a986e2d1c5
ra-fletcher <79005140+ra-fletcher@users.noreply.github.com>
2021-10-26 11:03:52 +0100

    Update looped_linear_regression.R

--- R/looped_linear_regression.R
+++ R/looped_linear_regression.R
@@ -11,11 +11,11 @@ run_multiple_lm_1 = function(df, outcomes) {
       dplyr::mutate(.y = {{ y }}) %>%
       lm(.y ~ indep_var, .) %>%
       broom::tidy() %>% 
-      dplyr::filter(!str_detect(term, "Intercept")) %>%
+      dplyr::filter(!stringr::str_detect(term, "Intercept")) %>%
       dplyr::select(variable = term, p_value = p.value) %>% 
       dplyr::mutate(
-        variable = df %>% select({{ y }}) %>% names(),
-        p_value = case_when(
+        variable = df %>% dplyr::select({{ y }}) %>% names(),
+        p_value = dplyr::case_when(
           p_value < 0.001 ~ "0.001",
           p_value > 0.001 ~ as.character(round(p_value, digits = 3))
         )
@@ -37,16 +37,20 @@ run_multiple_lm_2 = function(df, outcomes) {
   `%>%` = magrittr::`%>%`
   
   run_lm = function(df, y) {
-    response_var = enquo(y)
+    response_var = rlang::enquo(y)
     
     model_results = 
-      lm(expr(!!sym(quo_name(response_var)) ~ indep_var), data = df) %>%
+      lm(
+        rlang::expr(
+          !!rlang::sym(rlang::quo_name(response_var)) ~ indep_var
+        ), data = df
+      ) %>%
       broom::tidy() %>% 
-      filter(!str_detect(term, "Intercept")) %>%
-      select(variable = term, p_value = p.value) %>% 
-      mutate(
-        variable = df %>% select({{ y }}) %>% names(),
-        p_value = case_when(
+      dplyr::filter(!stringr::str_detect(term, "Intercept")) %>%
+      dplyr::select(variable = term, p_value = p.value) %>% 
+      dplyr::mutate(
+        variable = df %>% dplyr::select({{ y }}) %>% names(),
+        p_value = cdplyr::case_when(
           p_value < 0.001 ~ "0.001",
           p_value > 0.001 ~ as.character(round(p_value, digits = 3))
         )
